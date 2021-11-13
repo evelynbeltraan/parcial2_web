ae0666bf8805f5fef06b1df9406e1890434a0823
evelynbeltraan <94196811+evelynbeltraan@users.noreply.github.com>
2021-11-12 17:18:19 -0700

    Add files via upload

---
+++ practica1/practica1.php
@@ -0,0 +1,31 @@
+<?php 
+
+	echo "Hola mundo";
+	echo "<br><br>";
+
+	echo "<h1 style='color:red;'>Lista de numeros</h1>";
+
+	for ($i=0; $i < 10 ; $i++) { 
+		echo $i+1 . "<br>";
+	}
+
+	for ($i=0; $i < 10; $i++) { 
+		
+		echo "<div style='border: 1px solid green; padding:30px; width:30px; display:inline-flex'>";
+		echo $i+1;
+		echo "</div>";
+	}
+	echo "<br>";
+	for ($i=0; $i < 10 ; $i++) { 
+		
+	if ($i % 2 == 0){
+			echo "<div style='border: 1px solid green; padding:30px; width:30px; display:inline-flex; background-color:blue'>";
+		} else {
+			echo "<div style='border: 1px solid green; padding:30px; width:30px; display:inline-flex; background-color:orange'>";
+		}
+
+		echo $i+1;
+		echo "</div>";
+	}
+	echo "Sandoval Beltrán Evelyn Kassandra 5AVP"
+ ?>
